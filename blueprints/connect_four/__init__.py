6b22044e3066cae55729e0bd532d971f74543c1f
zhilin he <zhilinhe@usc-guestwireless-upc-newsc19032.usc.edu>
2022-12-01 18:15:12 -0800

    depth

--- blueprints/connect_four/__init__.py
+++ blueprints/connect_four/__init__.py
@@ -31,7 +31,7 @@ def get_solution():
         if method == "maxmin":
             game_board = decode_connect_four(board)
             game = MaxMin()
-            game.runMinMax(game_board, 5, number_to_win=number_to_win, http=True)
+            game.runMinMax(game_board, 6, number_to_win=number_to_win, http=True)
             data = game.output + 1
             print(data)
             return SuccessDataResponse(data)
